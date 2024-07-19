c832a2ab0567450951609f6c3c6a8e31b805b865
Justin Wang <justin@gmistudios.io>
2024-07-19 13:18:52 -0700

    Base

---
+++ src/app/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDPCNSsDiGs9VlS5HqJQJYvqaqbXUFvAO0",
+    authDomain: "twoscord-e3893.firebaseapp.com",
+    projectId: "twoscord-e3893",
+    storageBucket: "twoscord-e3893.appspot.com",
+    messagingSenderId: "632100975848",
+    appId: "1:632100975848:web:8daf3f69ef763dbc70055d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
